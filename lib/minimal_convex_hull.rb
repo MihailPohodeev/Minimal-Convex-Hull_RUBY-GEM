8c21e0853ae33431317ecf49523846390b64e778
MihailPohodeev <115114201+MihailPohodeev@users.noreply.github.com>
2024-06-02 14:27:27 +0300

    Revert "created gem files"
    This reverts commit 6d3bed68d145906a2a4cc90bf2c85576a569c36a.

--- lib/minimal_convex_hull.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "minimal_convex_hull/version"
-
-module MinimalConvexHull
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/minimal_convex_hull/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module MinimalConvexHull
-  VERSION = "0.1.0"
-end
