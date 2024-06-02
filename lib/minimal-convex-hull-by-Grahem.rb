7e2222bc73853f595e53822b8457cd049cd52088
MihailPohodeev <115114201+MihailPohodeev@users.noreply.github.com>
2024-06-02 16:17:52 +0300

    added Grahem's algorithm

--- lib/minimal-convex-hull-by-Grahem.rb
+++ lib/minimal-convex-hull-by-Grahem.rb
@@ -1,3 +1,40 @@
 class GrahemAlrotithm
-  #TODO
+private
+  def rotate(a, b, c)
+    return (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x)
+  end
+
+public
+
+  def convex_hull(points)
+    n = points.length
+    p = (0...n).to_a
+    for i in 0...n
+      if points[p[i]].x < points[p[0]].x
+        p[i], p[0] = p[0], p[i]
+      end
+    end
+    
+    for i in 2...n
+      j = i
+      while j>1 && (rotate(points[p[0]], points[p[j-1]], points[p[j]])<0) 
+        p[j], p[j-1] = p[j-1], p[j]
+        j -= 1
+      end
+    end
+    
+    s = [p[0], p[1]]
+    for i in 2...n
+      while rotate(points[s[-2]], points[s[-1]], points[p[i]]) < 0
+        s.pop
+      end
+      s << p[i]
+    end
+    
+    result = []
+    for i in 0..s.length-1
+      result << points[s[i]]
+    end
+    return result
+  end
 end
