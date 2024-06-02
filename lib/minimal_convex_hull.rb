6d3bed68d145906a2a4cc90bf2c85576a569c36a
MihailPohodeev <115114201+MihailPohodeev@users.noreply.github.com>
2024-06-02 12:16:12 +0300

    created gem files

---
+++ lib/minimal_convex_hull.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "minimal_convex_hull/version"
+
+module MinimalConvexHull
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/minimal_convex_hull/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MinimalConvexHull
+  VERSION = "0.1.0"
+end
