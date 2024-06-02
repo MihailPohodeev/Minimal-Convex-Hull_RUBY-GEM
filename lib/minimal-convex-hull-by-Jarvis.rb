856716f2cfd28b4ae5b6dac4c970c049e98fb02e
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-06-02 15:33:30 +0300

    функция алгоритма джарвиса

--- lib/minimal-convex-hull-by-Jarvis.rb
+++ lib/minimal-convex-hull-by-Jarvis.rb
@@ -1,3 +1,22 @@
 class JarvisAlrotithm
-  # TODO
+  def jarvis(points)
+    p0 = points.min_by { |p| [p.x, p.y] }
+    hull = [p0]
+    loop do
+      t = points[0]
+      points.each do |p|
+        if (p - p0).cross(t - p0) > 0
+          t = p
+        end
+      end
+      if t == p0
+        break
+      else
+        p0 = t
+        hull.push(t)
+      end
+    end
+    hull
+  end
+  
 end
